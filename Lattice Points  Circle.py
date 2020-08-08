f83b5b7b67b35fb2156867a4b5555f89e590add1
radhikaca <radhikaca@outlook.com>
2020-08-08 10:54:03 -0700

    Lattic Point Updated

--- Lattice Points  Circle.py
+++ Lattice Points  Circle.py
@@ -14,7 +14,7 @@ r=25
 
 
 count=0
->>>>>>> d40c517c0c5331f14bef0cfb8d0c58d857b0a3c9
+
 lp=[(0,r),(r,0),(0,-r),(-r,0)]
  # (0,r),(r,0),(0,-r),(-r,0)
 for i in range ( 1, r-1):
@@ -22,14 +22,8 @@ for i in range ( 1, r-1):
     y=int(m.sqrt(yy))
     if(y*y==yy):
         lp.extend([(y,i),(i,y),(-y,i),(i,-y)])
-
-
-print(lp)
-
-
-
         count+=1
-        
+
 
 print(lp)
 
